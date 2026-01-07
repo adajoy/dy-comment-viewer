489dd8829338b3d5725cb41c874e16eacae31edd
adajoy <adajoyyy@hotmail.com>
2026-01-07 14:08:10 +0800

    Set base path for Vite configuration and updated router basepath to '/comment-view'

--- src/router.tsx
+++ src/router.tsx
@@ -8,7 +8,7 @@ export const getRouter = () => {
   const router = createRouter({
     routeTree,
     context: {},
-
+    basepath: '/comment-view',
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
   })
